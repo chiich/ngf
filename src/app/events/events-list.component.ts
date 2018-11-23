df7e007c7c7abf0d01a82d050992cf2b648d8fb9
Chiemezie Ichongiri <Chiemezie.Ichongiri@wlt.com>
2018-11-23 16:54:29 +0000

    eom: Creating and Communicating Between Angular Components

---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'events-list',
+  template: `
+  <div>
+    <h1>Upcoming Angular Events</h1>
+    <hr>
+    <event-thumbnail #thumbnail [event]='eventX'></event-thumbnail>
+  </div>
+  `
+})
+
+export class EventsListComponent {
+  eventX = {
+    id: 1,
+    name: 'Angular Connect',
+    date: '9/26/2036',
+    time: '10:00 am',
+    price: 599.99,
+    imageUrl: '/assets/images/angualrconnect-shield.png',
+    location: {
+      address: '1057 DT',
+      city: 'London',
+      country: 'England'
+    }
+  }
+
+}
