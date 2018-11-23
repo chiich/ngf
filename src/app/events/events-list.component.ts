43a9cd0d08bfb0c2034ec3d84f56645ac5df5504
Chiemezie Ichongiri <Chiemezie.Ichongiri@wlt.com>
2018-11-23 17:36:28 +0000

    ngFor intro

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core'
   <div>
     <h1>Upcoming Angular Events</h1>
     <hr>
-    <event-thumbnail #thumbnail [event]='events'></event-thumbnail>
+    <event-thumbnail #thumbnail *ngFor="let event of events" [event]="event"></event-thumbnail>
   </div>
   `
 })
