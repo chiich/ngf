58a288da5663f2a316ab3b21cf2e9e0988d8678d
Chiemezie Ichongiri <Chiemezie.Ichongiri@wlt.com>
2018-11-27 11:45:08 +0000

    eom: Exploring the Angular Template Syntax

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,21 +4,38 @@ import { Component, Input } from '@angular/core'
   selector: 'event-thumbnail',
   template: `
     <div class="well hoverwell thumbnail">
-      <h2>{{event.name}}</h2>
-      <div>Date: {{event.date}}</div>
-      <div>Time: {{event.time}}</div>
-      <div>Price: \${{event.price}}</div>
+      <h2>{{event?.name}}</h2>
+      <div>Date: {{event?.date}}</div>
+      <div [ngClass]="getClassByTime()" [ngSwitch]="event?.time">
+        Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
+      <div>Price: \${{event?.price}}</div>
       <div>
-        <span>Location: {{event.location.address}}</span>
-        <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+        <span>Location: {{event?.location.address}}</span>
+        <span class="pad-left">{{event?.location.city}}, {{event?.location.country}}</span>
       </div>
     </div>
   `,
   styles: [`
-    .pad-left { margin-left: 10px; }
+  .thumbnail { min-height: 210px; }
+  .pad-left { margin-left: 10px; }
+  .well div { color: #bbb; }
+  .meat { color: greenyellow !important; }
+  .turkey { font-weight: bold; }
   `]
 })
 
 export class EventThumbnailComponent {
   @Input() event:any
+
+  getClassByTime() {
+    const isEarlyStart = this.event.time && this.event.time == '8:00 am'
+    if (isEarlyStart) {
+      return 'meat turkey'
+    }
+    return ''
+  }
 }
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core'
   <div>
     <h1>Upcoming Angular Events</h1>
     <hr>
-    <event-thumbnail #thumbnail *ngFor="let event of events" [event]="event"></event-thumbnail>
+    <div class="row">
+      <div class="col-md-5" *ngFor="let event of events" >
+        <event-thumbnail #thumbnail [event]="event"></event-thumbnail>
+      </div>
+    </div>
   </div>
   `
 })
@@ -17,7 +21,7 @@ export class EventsListComponent {
       id: 1,
       name: 'Angular Connect',
       date: '9/26/2036',
-      time: '10:00 am',
+      time: '8:00 am',
       price: 599.99,
       imageUrl: '/assets/images/angualrconnect-shield.png',
       location: {
@@ -60,7 +64,104 @@ export class EventsListComponent {
 
         }
       ]
+    },
+    {
+      id: 2,
+      name: 'Angular DisConnect',
+      date: '9/26/2036',
+      time: '10:00 am',
+      price: 799.99,
+      imageUrl: '/assets/images/angualrconnect-shield.png',
+      location: {
+        address: '1057 DT',
+        city: 'London',
+        country: 'England'
+      },
+      sessions: [
+        {
+          id: 1,
+          name: "Usng Angular 4 Pipes",
+          presenter: "Peter Bacon Darwin",
+          duration: 2,
+          level: "Intermediate",
+          abstract: `Learn all about the new pipes in Angular 4, both
+          how to write them and how to get the new AI CLI to write
+          them for you. Given by the famous PRD, president of Angular
+          University (formerly Oxford University)
+          `,
+          voters: ['bradgreen', 'igorminar', 'martinfowler']
+        },
+        {
+          id: 2,
+          name: "Getting the most out of your dev team",
+          presenter: "Jeff Gross",
+          duration: 4,
+          level: "Intermediate",
+          abstract: ``,
+          voters: ['johnpapa', 'bradgreen', 'igorminar', 'martinfowler']
+
+        },
+        {
+          id: 3,
+          name: "Angular 4 and Firebase",
+          presenter: "David East",
+          duration: 3,
+          level: "Beginner",
+          abstract: ``,
+          voters: ['bradgreen', 'igorminar']
+
+        }
+      ]
+    },
+    {
+      id: 3,
+      name: 'Angular InterConnect',
+      date: '9/26/2036',
+      time: '9:00 am',
+      price: 1049.99,
+      imageUrl: '/assets/images/angualrconnect-shield.png',
+      location: {
+        address: '1057 DT',
+        city: 'London',
+        country: 'England'
+      },
+      sessions: [
+        {
+          id: 1,
+          name: "Usng Angular 4 Pipes",
+          presenter: "Peter Bacon Darwin",
+          duration: 2,
+          level: "Intermediate",
+          abstract: `Learn all about the new pipes in Angular 4, both
+          how to write them and how to get the new AI CLI to write
+          them for you. Given by the famous PRD, president of Angular
+          University (formerly Oxford University)
+          `,
+          voters: ['bradgreen', 'igorminar', 'martinfowler']
+        },
+        {
+          id: 2,
+          name: "Getting the most out of your dev team",
+          presenter: "Jeff Gross",
+          duration: 4,
+          level: "Intermediate",
+          abstract: ``,
+          voters: ['johnpapa', 'bradgreen', 'igorminar', 'martinfowler']
+
+        },
+        {
+          id: 3,
+          name: "Angular 4 and Firebase",
+          presenter: "David East",
+          duration: 3,
+          level: "Beginner",
+          abstract: ``,
+          voters: ['bradgreen', 'igorminar']
+
+        }
+      ]
     }
+
   ]
 
 }
